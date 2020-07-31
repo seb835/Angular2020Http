a666f9fdafd7dcb3c8fd96fc6e80566e8b8be1ca
Seb Pedley <seb@propeller-studios.co.uk>
2020-07-31 16:14:09 +0100

    Loading indicator!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{[key: string]: Post}>('https://angular2020http.firebaseio.com/posts.json')
       .pipe(
@@ -53,6 +55,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       }
     );
