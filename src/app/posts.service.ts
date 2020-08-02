b28c6b2976572e8d4d0d31cc74d5b305f35c8072
Seb Pedley <nowt835@gmail.com>
2020-08-02 10:28:00 +0100

    Http events

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
+import { map, catchError, tap } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
 import { Post } from './post.model';
@@ -20,7 +20,10 @@ export class PostsService {
     return this.http
       .post<{ name: string }>(
         'https://angular2020http.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
       )
       .subscribe(
         responseData => {
@@ -63,6 +66,21 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete('https://angular2020http.firebaseio.com/posts.json');
+    return this.http.delete(
+      'https://angular2020http.firebaseio.com/posts.json',
+      {
+        observe: 'events'
+      }
+    ).pipe(tap(event => {
+      console.log(event);
+
+      if (event.type === HttpEventType.Sent) {
+        // ...
+      }
+
+      if (event.type === HttpEventType.Response) {
+        console.log(event.body);
+      }
+    }))
   }
 }
